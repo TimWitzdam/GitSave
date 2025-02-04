11e451ec91f7d637de4bb0af77c851aa3ccd1e7e
TimWitzdam <tim2013w@gmail.com>
2025-02-04 15:34:00 +0100

    Fixed first time setup schedule adding

--- src/components/Setup.tsx
+++ src/components/Setup.tsx
@@ -8,6 +8,12 @@ export default function Setup() {
   const [step, setStep] = React.useState(0);
   const [every, setEvery] = React.useState(1);
   const [loading, setLoading] = React.useState(false);
+  const [showAddAccessToken, setShowAddAccessToken] = React.useState(false);
+  const [privateRepo, setPrivateRepo] = React.useState(false);
+  const [accessToken, setAccessToken] = React.useState("default");
+  const [availableAccessTokens, setAvailableAccessTokens] = React.useState<
+    { id: number; name: string }[]
+  >([]);
 
   function updateEvery(e: React.ChangeEvent<HTMLInputElement>) {
     if (parseInt(e.target.value) < 1) {
@@ -73,6 +79,8 @@ export default function Setup() {
     const repository = formData.get("repository") as string;
     const every = parseInt(formData.get("every") as string);
     const timespan = formData.get("timespan") as string;
+    const privateRepo = formData.get("private") as string;
+    const accessTokenId = formData.get("access-token") as string;
 
     setLoading(true);
     fetch("/api/schedules", {
@@ -80,7 +88,14 @@ export default function Setup() {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ name, repository, every, timespan }),
+      body: JSON.stringify({
+        name,
+        repository,
+        every,
+        timespan,
+        private: privateRepo,
+        accessTokenId,
+      }),
     })
       .then((res) => {
         if (res.ok) {
@@ -93,13 +108,47 @@ export default function Setup() {
         setLoading(false);
         if (!data) return;
         setSuccess(
-          "Schedule created successfully. Redirecting to dashboard..."
+          "Schedule created successfully. Redirecting to dashboard...",
         );
         setTimeout(() => {
           setSuccess("");
           location.href = "/dashboard";
         }, 2000);
       });
+  }
+
+  function updateAccessToken(e: React.ChangeEvent<HTMLSelectElement>) {
+    if (e.target.value === "new") {
+      setShowAddAccessToken(true);
+      return;
+    }
+    setAccessToken(e.target.value);
+  }
+
+  function handleAccessTokenSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const form = e.currentTarget;
+    const formData = new FormData(form);
+    const name = formData.get("access-token-name") as string;
+    const token = formData.get("access-token") as string;
+
+    fetch("/api/access-tokens", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, token }),
+    }).then(() => {
+      form.reset();
+      setShowAddAccessToken(false);
+      fetchAccessTokens();
+    });
+  }
+
+  function fetchAccessTokens() {
+    fetch("/api/access-tokens")
+      .then((res) => res.json())
+      .then((data) => setAvailableAccessTokens(data));
   }
 
   return (
@@ -163,84 +212,159 @@ export default function Setup() {
       )}
       {step === 2 && (
         <div>
-          <h1 className="text-center font-medium text-2xl mb-6">
-            2. Adding first schedule
-          </h1>
-          <div className="w-fit ml-auto mb-6">
-            <BaseButton
-              type="secondary"
-              onClick={() => (location.href = "/dashboard")}
-            >
-              Skip for now
-            </BaseButton>
-          </div>
-          <form onSubmit={handleMonitorSubmit} action="submit">
-            <div className="flex flex-col gap-6">
-              <BaseInput
-                type="text"
-                label="Name"
-                placeholder="Enter schedule name"
-                name="name"
-                required
-              />
-              <BaseInput
-                type="url"
-                label="Repository link"
-                placeholder="https://github.com/exampleuser/examplerepo.git"
-                name="repository"
-                required
-              />
-              <div>
-                <label className="text-secondary">Backup routine</label>
-                <div className="flex flex-col gap-2 mt-2 sm:flex-row sm:items-center ">
-                  <div className="rounded-lg bg-bg-300 border-2 border-border-200 text-center py-3 px-4 text-secondary cursor-not-allowed">
-                    Every
-                  </div>
-                  <div className="w-fit">
-                    <BaseInput
-                      type="number"
-                      value={every}
-                      name="every"
-                      onChange={updateEvery}
-                      width="sm:w-24"
-                      required
-                    />
-                  </div>
-                  <select
-                    name="timespan"
-                    className="outline-none w-full rounded-lg bg-bg-300 border-2 border-border-200 text-center py-3 px-4 text-secondary focus:border-border-100 transition-colors"
-                  >
-                    <option value="minutes">
-                      Minute{every === 0 || every > 1 ? "s" : ""}
-                    </option>
-                    <option value="hours">
-                      Hour{every === 0 || every > 1 ? "s" : ""}
-                    </option>
-                    <option value="days">
-                      Day{every === 0 || every > 1 ? "s" : ""}
-                    </option>
-                  </select>
-                </div>
-              </div>
-              <BaseButton buttonType="submit">
-                <div className="flex items-center justify-center gap-2">
-                  {loading && (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      height="24px"
-                      viewBox="0 -960 960 960"
-                      width="24px"
-                      fill="#e8eaed"
-                      className="animate-spin"
-                    >
-                      <path d="M480-80q-82 0-155-31.5t-127.5-86Q143-252 111.5-325T80-480q0-83 31.5-155.5t86-127Q252-817 325-848.5T480-880q17 0 28.5 11.5T520-840q0 17-11.5 28.5T480-800q-133 0-226.5 93.5T160-480q0 133 93.5 226.5T480-160q133 0 226.5-93.5T800-480q0-17 11.5-28.5T840-520q17 0 28.5 11.5T880-480q0 82-31.5 155t-86 127.5q-54.5 54.5-127 86T480-80Z" />
-                    </svg>
-                  )}
-                  <span>Create schedule</span>
-                </div>
+          <div className={`${showAddAccessToken && "hidden"}`}>
+            <h1 className="text-center font-medium text-2xl mb-6">
+              2. Adding first schedule
+            </h1>
+            <div className="w-fit ml-auto mb-6">
+              <BaseButton
+                type="secondary"
+                onClick={() => (location.href = "/dashboard")}
+              >
+                Skip for now
               </BaseButton>
             </div>
-          </form>
+            <form onSubmit={handleMonitorSubmit} action="submit">
+              <div className="flex flex-col gap-6">
+                <BaseInput
+                  type="text"
+                  label="Name"
+                  placeholder="Enter schedule name"
+                  name="name"
+                  required
+                />
+                <div>
+                  <span className="text-secondary">Repository</span>
+                  <div className="mt-2">
+                    <BaseInput
+                      type="url"
+                      placeholder="https://github.com/exampleuser/examplerepo.git"
+                      name="repository"
+                      required
+                    />
+                    <div className="flex items-center gap-2 mt-2">
+                      <div className="flex items-center gap-2 rounded-lg bg-bg-300 border-2 border-border-200 text-center py-3 px-4 text-secondary">
+                        <label htmlFor="private">Private?</label>
+                        <input
+                          type="checkbox"
+                          name="private"
+                          id="private"
+                          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                            setPrivateRepo(e.target.checked)
+                          }
+                        />
+                      </div>
+                      {privateRepo && (
+                        <select
+                          name="access-token"
+                          onChange={updateAccessToken}
+                          value={accessToken}
+                          className="outline-none w-full rounded-lg bg-bg-300 border-2 border-border-200 py-3 px-4 text-secondary focus:border-border-100 transition-colors"
+                        >
+                          <option disabled value="default">
+                            Select an option
+                          </option>
+                          {availableAccessTokens.map((token) => (
+                            <option key={token.id} value={token.id}>
+                              {token.name}
+                            </option>
+                          ))}
+                          <option value="new">+ Add new access token</option>
+                        </select>
+                      )}
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <label className="text-secondary">Backup routine</label>
+                  <div className="flex flex-col gap-2 mt-2 sm:flex-row sm:items-center ">
+                    <div className="rounded-lg bg-bg-300 border-2 border-border-200 text-center py-3 px-4 text-secondary cursor-not-allowed">
+                      Every
+                    </div>
+                    <div className="w-fit">
+                      <BaseInput
+                        type="number"
+                        value={every}
+                        name="every"
+                        onChange={updateEvery}
+                        width="sm:w-24"
+                        required
+                      />
+                    </div>
+                    <select
+                      name="timespan"
+                      className="outline-none w-full rounded-lg bg-bg-300 border-2 border-border-200 text-center py-3 px-4 text-secondary focus:border-border-100 transition-colors"
+                    >
+                      <option value="minutes">
+                        Minute{every === 0 || every > 1 ? "s" : ""}
+                      </option>
+                      <option value="hours">
+                        Hour{every === 0 || every > 1 ? "s" : ""}
+                      </option>
+                      <option value="days">
+                        Day{every === 0 || every > 1 ? "s" : ""}
+                      </option>
+                    </select>
+                  </div>
+                </div>
+                <BaseButton buttonType="submit">
+                  <div className="flex items-center justify-center gap-2">
+                    {loading && (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        height="24px"
+                        viewBox="0 -960 960 960"
+                        width="24px"
+                        fill="#e8eaed"
+                        className="animate-spin"
+                      >
+                        <path d="M480-80q-82 0-155-31.5t-127.5-86Q143-252 111.5-325T80-480q0-83 31.5-155.5t86-127Q252-817 325-848.5T480-880q17 0 28.5 11.5T520-840q0 17-11.5 28.5T480-800q-133 0-226.5 93.5T160-480q0 133 93.5 226.5T480-160q133 0 226.5-93.5T800-480q0-17 11.5-28.5T840-520q17 0 28.5 11.5T880-480q0 82-31.5 155t-86 127.5q-54.5 54.5-127 86T480-80Z" />
+                      </svg>
+                    )}
+                    <span>Create schedule</span>
+                  </div>
+                </BaseButton>
+              </div>
+            </form>
+          </div>
+          <div className={`${!showAddAccessToken && "hidden"}`}>
+            <h2 className="text-2xl font-medium mb-4">Add access token</h2>
+            <form action="submit" onSubmit={handleAccessTokenSubmit}>
+              <div className="flex flex-col gap-6">
+                <a
+                  className="text-secondary underline hover:text-white transition-colors"
+                  href="https://github.com/TimWitzdam/GitSave/wiki/How-to-create-a-GitHub-access-token"
+                  target="_blank"
+                >
+                  How to create a GitHub access token
+                </a>
+                <BaseInput
+                  type="text"
+                  label="Name"
+                  placeholder="Enter access token name"
+                  name="access-token-name"
+                  required
+                />
+                <BaseInput
+                  type="password"
+                  label="Token"
+                  placeholder="Enter access token"
+                  name="access-token"
+                  required
+                />
+              </div>
+              <div className="mt-6 flex items-center justify-between gap-2">
+                <BaseButton
+                  type="secondary"
+                  buttonType="reset"
+                  onClick={() => setShowAddAccessToken(false)}
+                >
+                  Go back
+                </BaseButton>
+                <BaseButton buttonType="submit">Add access token</BaseButton>
+              </div>
+            </form>
+          </div>
         </div>
       )}
       {error && <p className="text-red text-center mt-4">{error}</p>}
